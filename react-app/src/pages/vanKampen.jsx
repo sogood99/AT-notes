f686ad46759e3aa7441bef2e41efd8991c9d9e64
Onii Chan <46487694+sogood99@users.noreply.github.com>
2022-01-05 17:03:04 +0800

    Added in commutative diagram for van kampen

--- react-app/src/pages/vanKampen.jsx
+++ react-app/src/pages/vanKampen.jsx
@@ -14,7 +14,22 @@ export default function VanKampen() {
         Van Kampen Theorem and Applications
       </Typography>
       <div className="theorem"> (Van Kampen's Theorem) {vanKampen}</div>
-      <div className="tex">{vanKampenExplain}</div>
+      <div className="tex">
+        {vanKampenExplain} When the there are exactly two open covers then the
+        path connectedness is vacuously satisfied and it yields this nice
+        commutative diagram from
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://en.wikipedia.org/wiki/Seifert%E2%80%93Van_Kampen_theorem"
+        >
+          Wikipedia
+        </a>
+        .
+      </div>
+      <div>
+        <img src="fig/VanKampen.png" alt="Van Kampen Commutative Diagram" />
+      </div>
       <Link to="/">
         <Button>Back</Button>
       </Link>
